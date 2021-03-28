d33b42d6a62ccfc0ac5c1caedaff1303382d19fc
yiwen950725 <tiffany-gu@hotmail.com>
2021-03-28 00:46:25 -0700

    test

--- Submitted/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 81;
-app.use(express.static(__dirname))
-app.get('/', (req, res) => res.sendFile(__dirname+'/static/html/index.html'))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- Submitted/readme.md
+++
@@ -1,6 +0,0 @@
-## Mosaic x Hatchways
-Make sure you install node already.    
-```Open the terminal, run node app.js. ```   
-It automatically runs on port 81. Open the browser (Google Chrome recommended).   
-Type    
-```http://localhost:81/```
